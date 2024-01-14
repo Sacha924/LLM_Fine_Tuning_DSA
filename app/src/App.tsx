17d210802467da5feb6f9fccb8a040ef69b4f8f8
Sacha924 <24448@student.dorset-college.ie>
2024-01-14 11:42:57 +0100

    Clean style for the app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import './App.css'; // Ensure you have this CSS file
+import './App.css';
 
 function App() {
   const ws = React.useRef<WebSocket | null>(null);
   const [messages, setMessages] = useState<{ sender: string, content: string }[]>([]);
   const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     ws.current = new WebSocket('ws://localhost:8080');
 
     ws.current.onmessage = async (event) => {
+      setLoading(false)
       const newMessage = typeof event.data === 'string' ? event.data : await event.data.text();
       setMessages(prevMessages => [...prevMessages, { sender: 'ai', content: newMessage }]);
     };
@@ -26,6 +28,7 @@ function App() {
     if (message !== "") {
       setMessages(prevMessages => [...prevMessages, { sender: 'user', content: message }]);
       ws.current?.send(message);
+      setLoading(true);
       setMessage("");
     }
   }
@@ -33,7 +36,7 @@ function App() {
   return (
     <div className="App">
       <h1>Coding Chat</h1>
-      <p> Ask Your Coding DSA question and get your answer</p>
+      <p>Ask Your Coding DSA question and get your answer</p>
       <div className="messages">
         {messages.map((msg, index) => (
           <div key={index} className={`message ${msg.sender}`}>
@@ -44,18 +47,24 @@ function App() {
             }
           </div>
         ))}
-      </div>
-      <div className="chat-input-container">
-        <form onSubmit={handleSubmit}>
-          <input
-            type='text'
-            value={message}
-            onChange={e => setMessage(e.target.value)} />
-          <button type="submit">Send</button>
-        </form>
+        {
+          loading && (
+            <div className="loading-indicator">
+            </div>
+          )
+        }
+        <div className="chat-input-container">
+          <form onSubmit={handleSubmit} className="input-form">
+            <input
+              type='text'
+              value={message}
+              onChange={e => setMessage(e.target.value)}
+              disabled={loading} />
+            <button type="submit">Send</button>
+          </form>
+        </div>
       </div>
     </div>
-
   );
 }
 
